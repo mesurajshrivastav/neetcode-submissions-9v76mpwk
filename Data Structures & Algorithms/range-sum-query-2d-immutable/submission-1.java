ac246cf6e0a39ae5086bd2ccef5ac042177a038f
Suraj Shrivastav <109119656+mesurajshrivastav@users.noreply.github.com>
2026-05-13 15:01:50 +0530

    Add: range-sum-query-2d-immutable - submission-1

---
+++ Data Structures & Algorithms/range-sum-query-2d-immutable/submission-1.java
@@ -0,0 +1,32 @@
+class NumMatrix {
+
+int prefixSum[][];
+    public NumMatrix(int[][] matrix) {
+        int m = matrix.length;
+        int n = matrix[0].length;
+
+         prefixSum = new int[m][n];
+         for(int i=0; i<m; i++){
+            for(int j=0; j<n; j++){
+                int top = (i > 0 ) ? prefixSum[i-1][j] : 0;
+                int left = (j > 0 ) ? prefixSum[i][j-1] : 0;
+                int topLeft = (i > 0  && j> 0) ? prefixSum[i-1][j-1] : 0;
+                prefixSum[i][j] = matrix[i][j]+ top+ left - topLeft; 
+            }
+         }
+    }
+    public int sumRegion(int row1, int col1, int row2, int col2) {
+
+        int total = prefixSum[row2][col2];
+        int top = (row1 > 0) ?  prefixSum[row1-1][col2] : 0;
+        int left = (col1 > 0) ? prefixSum[row2][col1-1] : 0;
+        int topLeft = (row1 > 0 && col1 > 0) ?prefixSum[row1-1][col1-1] : 0;
+        return total-top-left+topLeft;
+    }
+}
+
+/**
+ * Your NumMatrix object will be instantiated and called as such:
+ * NumMatrix obj = new NumMatrix(matrix);
+ * int param_1 = obj.sumRegion(row1,col1,row2,col2);
+ */
